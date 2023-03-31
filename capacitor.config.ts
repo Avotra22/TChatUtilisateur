14747493a509faa29c17d60b210f9268dcaf2e25
avotra <avotrarakotonarivoo@gmail.com>
2023-04-01 00:39:56 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TChat',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
